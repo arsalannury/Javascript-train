c620d68f3c80e8a24685d95187bc341d67a43c90
arsalannury <arsiiiland@yahoo.com>
2022-08-25 14:05:39 -0700

    Converter Method -string

---
+++ app/app.js
@@ -0,0 +1,25 @@
+function Converter(value, valueType) {
+  if (typeof valueType !== "string") {
+    throw new Error(`type string is not assinble to type ${typeof valueType}`);
+  }
+
+  const valueTypeToLowerCase = valueType.toLowerCase();
+
+  if (valueTypeToLowerCase === "string") {
+    if (typeof value === "number") {
+      return String(value);
+    } else if (Array.isArray(value)) {
+      return value.join(" ");
+    } else if (typeof value === "boolean") {
+      return String(value);
+    } else {
+      throw new Error("only number and boolean type can changed to string");
+    }
+  }
+
+  
+
+  
+}
+
+// console.log(Converter([34,56], "string"));
