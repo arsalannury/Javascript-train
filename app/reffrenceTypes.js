339b9d2526c1c9b63b9c5e98a8509c22ead6d645
arsalannury <arsiiiland@yahoo.com>
2022-09-05 12:26:05 +0430

    reform method

--- app/reffrenceTypes.js
+++ app/reffrenceTypes.js
@@ -1,25 +1,25 @@
-const _isArray = new WeakMap();
 
 function ConverterReffrence (objectOrArray) {
 
    const validArrayValues = ['1','2','3','4','5','6','7','8','9','0','true','false',true,false,1,2,3,4,5,6,7,8,9,0];
 
     if(typeof objectOrArray !== "object" || !Array.isArray(objectOrArray)) {
-        throw new Error(`type ${typeof objectOrArray} is not assignable to type Object or Array`)
+        throw new Error(`type ${typeof objectOrArray} is not assignable to type Object or Array`);
     }
+    if(objectOrArray.length === 0) return []; 
 
-    if(Array.isArray(objectOrArray)) {
-        let isArray = objectOrArray;
-        if(isArray.length === 0) return []; 
-        const r= objectOrArray.every((value) => value === 1)
-        console.log(r);
-        // const deleteDuplicateValues = [...new Set(objectOrArray)]; 
-        // console.log(deleteDuplicateValues);
-      
-        // const e = objectOrArray.filter((value,index) => value.toString().includes(validArrayValues[index]));
-        // console.log(e);
-        
-    }
+    // if(Array.isArray(objectOrArray)) {
+        // let isArray = objectOrArray;
+        // validArrayValues.forEach((num,index) => {
+        //    const resultFilteringValues = objectOrArray.filter(value => value === num);
+        // })   
+        for(let iterate = 0 ; iterate <= validArrayValues.length ; iterate++) {
+            const resultFilteringValues = objectOrArray.filter(value => value === validArrayValues[iterate]);
+            if(iterate === validArrayValues.length) {
+                console.log(resultFilteringValues) 
+            }
+        }     
+    // }
 
 }
 
