2483283d2b8e62b3c585a7332fa4a08267bbf0c6
arsalannury <arsiiiland@yahoo.com>
2022-09-05 17:13:29 +0430

    fixed bug

--- app/reffrenceTypes.js
+++ app/reffrenceTypes.js
@@ -1,26 +1,22 @@
+function ConverterReffrence(objectOrArray) {
+  
+    const validArrayValues = ['1','2','3','4','5','6','7','8','9','0','true','false',true,false,1,2,3,4,5,6,7,8,9,0];
 
-function ConverterReffrence (objectOrArray) {
+  if (typeof objectOrArray !== "object" || !Array.isArray(objectOrArray)) {
+    throw new Error(
+      `type ${typeof objectOrArray} is not assignable to type Object or Array`
+    );
+  }
+  if (objectOrArray.length === 0) return [];
 
-   const validArrayValues = ['1','2','3','4','5','6','7','8','9','0','true','false',true,false,1,2,3,4,5,6,7,8,9,0];
+  const includesFn = (value) => {
+    return validArrayValues.includes(value);
+  };
 
-    if(typeof objectOrArray !== "object" || !Array.isArray(objectOrArray)) {
-        throw new Error(`type ${typeof objectOrArray} is not assignable to type Object or Array`);
-    }
-    if(objectOrArray.length === 0) return []; 
-
-    // if(Array.isArray(objectOrArray)) {
-        // let isArray = objectOrArray;
-        // validArrayValues.forEach((num,index) => {
-        //    const resultFilteringValues = objectOrArray.filter(value => value === num);
-        // })   
-        for(let iterate = 0 ; iterate <= validArrayValues.length ; iterate++) {
-            const resultFilteringValues = objectOrArray.filter(value => value === validArrayValues[iterate]);
-            if(iterate === validArrayValues.length) {
-                console.log(resultFilteringValues) 
-            }
-        }     
-    // }
-
+  if (Array.isArray(objectOrArray)) {
+    const res = objectOrArray.filter(includesFn);
+    console.log(res);
+  }
 }
 
-console.log(ConverterReffrence(['1','2','r','t',2,"5","3"]));
+console.log(ConverterReffrence(["1", "2", "r", "t", 2, "5", "3"]));
