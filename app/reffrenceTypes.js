00efb67078243363f2f5d3194a847912c06758df
arsalannury <arsiiiland@yahoo.com>
2022-09-06 11:41:21 +0430

    add sorting function to ConverterReffrence

--- app/reffrenceTypes.js
+++ app/reffrenceTypes.js
@@ -1,5 +1,5 @@
 function ConverterReffrence(objectOrArray) {
-  // * * * if argument whitch is passed was a Array , Converter using this array for validating values * * * //
+  // * * * if argument which is passed was a Array , Converter using this array for validating values * * * //
   const validArrayValues = ['1','2','3','4','5','6','7','8','9','0',true,false,1,2,3,4,5,6,7,8,9,0];
   if (typeof objectOrArray !== "object" || !Array.isArray(objectOrArray)) {
     throw new Error(
@@ -12,16 +12,19 @@ function ConverterReffrence(objectOrArray) {
     const includesFn = (value) => {
       return validArrayValues.includes(value);
     };
-    const iterateFinalFn = (value) => {
+    const mapOnFinalArrayFn = (value) => {
       const toNumber = Number(value);
       return toNumber;
     };
+    const sortArrayFn = (a,b) => {
+      return a - b
+    }
     const FinalValidArrayValues = objectOrArray.filter(includesFn);
-    const FinalValidArrayValuesToNumber = FinalValidArrayValues.map(iterateFinalFn)
-    console.log(FinalValidArrayValuesToNumber);
-    // const removeDuplicateValues = [...new Set(FinalValidArrayValues)];
-    // console.log(removeDuplicateValues);
+    const FinalValidArrayValuesToNumber = FinalValidArrayValues.map(mapOnFinalArrayFn)
+    const removeDuplicateValues = [...new Set(FinalValidArrayValuesToNumber)];
+    const sortArray = removeDuplicateValues.sort(sortArrayFn)
+    return sortArray;
   }
 }
 
-ConverterReffrence([false,"true","1", "2", "2", "2", "2", "2", "2", "2", "r", "t", 2, "5", "3"])
+console.log(ConverterReffrence(["true","1", "2", "2", "2", "2", "2",false, "2", "2", "r", "t", 2, "5", "3"]));
