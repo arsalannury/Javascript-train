32f77bc923bd5a03295a617e7a173ef202c1c70f
arsalannury <arsiiiland@yahoo.com>
2022-09-26 15:00:45 +0330

    [add new file] math class added start

--- app/Math.js
+++ app/Math.js
@@ -1,5 +1,14 @@
-class Math {
+class Rectangle {
     static measurementRectangle (latitude,longitude) {
-       
+       return latitude * longitude
+    }
+
+    static ambianceRectangle (latitude,longitude) {
+        return (latitude * longitude) + 2
+    }
+
+    static rectangularCubeVolume (latitude,longitude,height) {
+        return (latitude * longitude * height)
     }
 }
+
