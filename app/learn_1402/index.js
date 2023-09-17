dd6ac9cd28d7a2e89cf07198b3ac185aa99c62d3
arsalannury <arsiiiland@yahoo.com>
2023-09-17 21:45:24 +0330

    train -- for of and for in

--- app/learn_1402/index.js
+++ app/learn_1402/index.js
@@ -3,6 +3,11 @@ let flatArray = array.flat();
 let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let flatMapNumbers = arrayNumbers.flatMap((value => value * 2));
 let i = 0;
+let arr = [];
+let object = {
+    model: 'BMW',
+    origin: 'Germany'
+}
 
 /*for (let i = 0; i <= 10; i++) {
    if(i%2 !== 0) {
@@ -19,11 +24,23 @@ let i = 0;
     console.log(i)
 }*/
 
-do {
+/*do {
     i++
     if(i%2 !== 0) {
         continue;
     }
     console.log(i)
-} while (i <= 10)
+} while (i <= 10)*/
+
+
+/*
+for(let month of array.flat())
+    arr.push(month);
+
+
+console.log(arr)*/
+
+for (let obj of object)
+    console.log(obj)
+
 
